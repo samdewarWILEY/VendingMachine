d89602a4bd7afd5a6c2d2cadc9df17a232c603ca
Ope Omiwade <opeomiwade21@gmail.com>
2024-03-20 14:54:28 +0000

    dto added

--- src/packages/dto/VendingMachineItem.java
+++ src/packages/dto/VendingMachineItem.java
@@ -1,4 +1,35 @@
 package packages.dto;
 
+import packages.view.VendingMachineView;
+
 public class VendingMachineItem {
+    private String name;
+    private double cost;
+    private int quantity;
+
+    public VendingMachineItem(String name, double cost, int quantity){
+        this.cost= cost;
+        this.name = name;
+        this.quantity =  quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }

8e9027b949cee55b9950cd9d28113439d5d21d49
Sam Dewar <sdewar@wiley.com>
2024-03-20 15:07:47 +0000

    IO implementation

--- src/packages/io/UserIO.java
+++ src/packages/io/UserIO.java
@@ -1,3 +1,7 @@
 package packages.io;
 public interface UserIO {
+    public void printString(String message);
+    public double getDouble(String message);
+    public int getInt(String message);
+    public String getString(String message);
 }
--- src/packages/io/UserIOImpl.java
+++ src/packages/io/UserIOImpl.java
@@ -1,4 +1,50 @@
 package packages.io;
 
-public class UserIOImpl {
+import java.util.Scanner;
+
+public class UserIOImpl implements UserIO{
+    Scanner scanner;
+    public UserIOImpl(){
+        this.scanner=new Scanner(System.in);
+    }
+    public UserIOImpl(Scanner scanner){
+        this.scanner=scanner;
+    }
+    @Override
+    public void printString(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public double getDouble(String message) {
+        System.out.println(message);
+        try{
+            return scanner.nextDouble();
+        }catch (Exception e){
+            System.out.println(e);
+            return 0;
+        }
+    }
+
+    @Override
+    public int getInt(String message) {
+        System.out.println(message);
+        try{
+            return scanner.nextInt();
+        }catch (Exception e){
+            System.out.println(e);
+            return 0;
+        }
+    }
+
+    @Override
+    public String getString(String message) {
+        System.out.println(message);
+        try{
+            return scanner.nextLine();
+        }catch (Exception e){
+            System.out.println(e);
+            return "";
+        }
+    }
 }

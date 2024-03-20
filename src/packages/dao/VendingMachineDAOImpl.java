8a3fff29f64108238fb85935d0cc0fa98e786758
ZhimengCh <zhchen@wiley.com>
2024-03-20 16:14:26 +0000

    unmarshalling Items

--- src/packages/dao/VendingMachineDAOImpl.java
+++ src/packages/dao/VendingMachineDAOImpl.java
@@ -1,16 +1,28 @@
 package packages.dao;
 
+import java.io.*;
+import java.math.BigDecimal;
 import java.util.*;
 
 public class VendingMachineDAOImpl implements VendingMachineDAO{
 
     private Map<String,VendingMachineItem> items;
-    private final String filePath = "packages/dao/data.txt";
+    private final String filePath = "data.txt";
+    private FileReader fileReader;
+    private FileWriter fileWriter;
+    private BufferedReader bufferedReader;
+    private final String delimeter = "::";
+
+    public VendingMachineDAOImpl() {
+        items = new HashMap<>();
+    }
 
     @Override
     public Map<String, VendingMachineItem> getAllItems() {
         return this.items;
     }
+
+
 
     @Override
     public void setAllItems(Map<String, VendingMachineItem> items) {
@@ -24,7 +36,23 @@ public class VendingMachineDAOImpl implements VendingMachineDAO{
 
     @Override
     public void unmarshallItems() {
-
+        try {
+            fileReader = new FileReader(filePath);
+            bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine())!= null){
+                String[] tokens = line.split(delimeter);
+                String name = tokens[0];
+                BigDecimal cost = new BigDecimal(tokens[1]);
+                int qty = Integer.parseInt(tokens[2]);
+                VendingMachineItem item = new VendingMachineItem(name,cost,qty);
+                items.put(name,item);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override

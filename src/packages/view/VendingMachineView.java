ff32858c87b9348d2579e22739b21e56d54ba993
Ope Omiwade <opeomiwade21@gmail.com>
2024-03-20 15:09:59 +0000

    view

--- src/packages/view/VendingMachineView.java
+++ src/packages/view/VendingMachineView.java
@@ -1,3 +1,31 @@
 package packages.view;
+
+import packages.dto.VendingMachineItem;
+import packages.io.UserIO;
+
+import java.util.Map;
+
 public class VendingMachineView {
+    private UserIO io;
+
+    public VendingMachineView(UserIO io) {
+        this.io = io;
+    }
+
+    public void displayAllItems(Map<String, VendingMachineItem> items) {
+        int i = 0; // to assign a number to each item.
+        for (VendingMachineItem item : items.values()) {
+            io.print(i + ". " + item.getName() + "::" + item.getCost());
+            i++;
+        }
+        io.print(items.size() + " .Exit");
+    }
+
+    public double promptToInsertCoin() {
+        return io.readDouble("Please put a coin into the vending machine: ",  0, 100 );
+    }
+
+    public int promptForItem() {
+        return io.readInt("Please choose an item from the list above.");
+    }
 }

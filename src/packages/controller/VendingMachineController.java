60d67e2cbf9690a180c1c4b2023e8ebae53c767a
Sam Dewar <sdewar@wiley.com>
2024-03-20 16:44:26 +0000

    Controller run()

--- src/packages/controller/VendingMachineController.java
+++ src/packages/controller/VendingMachineController.java
@@ -1,3 +1,27 @@
 package packages.controller;
+
+import packages.dao.VendingMachineDAO;
+import packages.service.VendingMachineServiceLayer;
+import packages.view.VendingMachineView;
+
 public class VendingMachineController {
+
+    VendingMachineDAO dao;
+    VendingMachineServiceLayer serviceLayer;
+    VendingMachineView view;
+
+    VendingMachineController(VendingMachineDAO dao, VendingMachineServiceLayer serviceLayer, VendingMachineView view) {
+        this.dao = dao;
+        this.serviceLayer = serviceLayer;
+        this.view = view;
+    }
+
+    public void run() {
+        while (true) {
+            view.displayAllItems(dao.getAllItems());
+            view.promptForItem();
+        }
+    }
+
+
 }
